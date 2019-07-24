e395788459830bd52024866a9ec2fcb3aa5aec65
Kevin Peters <kevin.peters@klarna.com>
2019-07-24 11:15:13 +0200

    feat: apply license

--- klarna/config.py
+++ klarna/config.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 
+# Copyright 2019-present Klarna AB
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Example config"""
-
-
 API_KEY = 'test_d4ca9ed6-489b-42b3-8011-dacdcee0fdb6'
-API_SECRET = 'test_87b858984787cf3fff2281e533a400ac7146d78f4a8b7af0709d673e' + \
-             '70d744ec'
-SECRET_KEY = 'secret' # Really?
+API_SECRET = 'test_87b858984787cf3fff2281e533a400ac7146d78f4a8b7af0709d673e70d744ec'
+SECRET_KEY = 'secret'
